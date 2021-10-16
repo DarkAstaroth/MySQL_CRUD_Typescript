3799f1e37167b54e3ad7a4e98e6c55ac8184870d
Manuel <mninahuanca4@gmail.com>
2021-10-16 17:18:15 -0400

    Crear el servidor de express y sus middlewares

--- app.ts
+++ app.ts
@@ -1,2 +1,9 @@
-export const nombre = "Manuel Ninahuanca";
-console.log(nombre);
+import dotenv from "dotenv";
+import Server from "./models/models";
+
+// Configurar dotenv
+dotenv.config();
+
+const server = new Server();
+
+server.listen();
